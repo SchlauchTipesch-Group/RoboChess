f3ad492cb2782dadd28c25b57bc541d4cb429a95
Dror Chen <dror.chen.2006@gmail.com>
2024-03-11 18:31:31 +0200

    Feat (StepperControl.cpp): Added validation for targetSquare.

--- Arduino/main/SteppersControl.cpp
+++ Arduino/main/SteppersControl.cpp
@@ -3,79 +3,83 @@
 
 SteppersControl::SteppersControl(const STEP_PINS xAxisStepPin, const DIR_PINS xAxisDirPin, const STEP_PINS yAxisStepPin, const DIR_PINS yAxisDirPin)
 {
-  _xAxisStepPin = xAxisStepPin;
-  _xAxisDirPin = xAxisDirPin;
-  _yAxisStepPin = yAxisStepPin;
-  _yAxisDirPin = yAxisDirPin;
+    _xAxisStepPin = xAxisStepPin;
+    _xAxisDirPin = xAxisDirPin;
+    _yAxisStepPin = yAxisStepPin;
+    _yAxisDirPin = yAxisDirPin;
 
-  // TODO: Add calibration function
-  _xAxisLocation = 0;
-  _yAxisLocation = 0;
-  _currentSquare = 0;
+    // TODO: Add calibration function
+    _xAxisLocation = 0;
+    _yAxisLocation = 0;
+    _currentSquare = 0;
 
-  // Turn on the enable pin
-  pinMode(EN_PIN, OUTPUT);
- 	digitalWrite(EN_PIN, HIGH);
+    // Turn on the enable pin
+    pinMode(EN_PIN, OUTPUT);
+    digitalWrite(EN_PIN, HIGH);
 
- 	pinMode(_xAxisStepPin, OUTPUT);
- 	pinMode(_xAxisDirPin, OUTPUT);
-  pinMode(_yAxisStepPin, OUTPUT);
- 	pinMode(_yAxisDirPin, OUTPUT);
+    pinMode(_xAxisStepPin, OUTPUT);
+    pinMode(_xAxisDirPin, OUTPUT);
+    pinMode(_yAxisStepPin, OUTPUT);
+    pinMode(_yAxisDirPin, OUTPUT);
 }
 
 void SteppersControl::goToSquare(const int targetSquare)
 {
-  int targetRow = targetSquare % 8;
-  int targetCol = targetSquare / 8;
+    if (targetSquare < 0 || targetSquare > 63)
+    {
+        return;
+    }
+    int targetRow = targetSquare % 8;
+    int targetCol = targetSquare / 8;
 
-  move(targetRow > _currentSquare % 8,
-    targetRow * SQUARE_SIZE,
-    targetCol > _currentSquare / 8,
-    targetCol * SQUARE_SIZE
-  );
+    move(targetRow > _currentSquare % 8,
+        targetRow * SQUARE_SIZE,
+        targetCol > _currentSquare / 8,
+        targetCol * SQUARE_SIZE
+    );
 
-  _currentSquare = targetSquare;
+    _currentSquare = targetSquare;
 }
 
 void SteppersControl::move(const bool xAxisDirection, const int xAxisDistance, const bool yAxisDirection, const int yAxisDistance)
 {
-  // Declaring the direction for each axis
-  digitalWrite(_xAxisDirPin, xAxisDirection);
-  digitalWrite(_yAxisDirPin, yAxisDirection);
+    // Declaring the direction for each axis
+    digitalWrite(_xAxisDirPin, xAxisDirection);
+    digitalWrite(_yAxisDirPin, yAxisDirection);
 
-  // Run until both axes move their target distance
-  for (int i = 0; (i < X_STEPS_PER_MM * xDistance) || (i < Y_STEPS_PER_MM * yDistance); i++)
-  {
-    // If the X axis has more to go, set step pin to HIGH
-    if (i < X_STEPS_PER_MM * xDistance)
+    // Run until both axes move their target distance
+    for (int i = 0; (i < X_STEPS_PER_MM * xDistance) || (i < Y_STEPS_PER_MM * yDistance); i++)
     {
-      digitalWrite(_xAxisStepPin, HIGH);
-    }
-    // Else, set step pin to LOW so that it wouldn't interfere with the other axis
-    else
-    {
-      digitalWrite(_xAxisStepPin, LOW);
-    }
-    
-    // If the Y axis has more to go, set step pin to HIGH
-    if (i < Y_STEPS_PER_MM * yDistance)
-    {
-      digitalWrite(_yAxisStepPin, HIGH);
-    }
-    // Else, set step pin to LOW so that it wouldn't interfere with the other axis
-    else
-    {
-      digitalWrite(_yAxisStepPin, LOW);
+        // If the X axis has more to go, set step pin to HIGH
+        if (i < X_STEPS_PER_MM * xDistance)
+        {
+            digitalWrite(_xAxisStepPin, HIGH);
+        }
+        // Else, set step pin to LOW so that it wouldn't interfere with the other axis
+        else
+        {
+            digitalWrite(_xAxisStepPin, LOW);
+        }
+
+        // If the Y axis has more to go, set step pin to HIGH
+        if (i < Y_STEPS_PER_MM * yDistance)
+        {
+            digitalWrite(_yAxisStepPin, HIGH);
+        }
+        // Else, set step pin to LOW so that it wouldn't interfere with the other axis
+        else
+        {
+            digitalWrite(_yAxisStepPin, LOW);
+        }
+
+        // Delay and set both step pins to LOW
+        delay(5);
+        digitalWrite(_xAxisStepPin, LOW);
+        digitalWrite(_yAxisStepPin, LOW);
+        delay(5);
     }
 
-    // Delay and set both step pins to LOW
-    delay(5);
-    digitalWrite(_xAxisStepPin, LOW);
-    digitalWrite(_yAxisStepPin, LOW);
-    delay(5);
-  }
-
-  // Update the current location of each axis
-  _xAxisLocation += xAxisDistance;
-  _yAxisLocation += yAxisDistance;
+    // Update the current location of each axis
+    _xAxisLocation += xAxisDistance;
+    _yAxisLocation += yAxisDistance;
 }
